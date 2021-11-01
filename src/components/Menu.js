c56701c2c0640095a8c51e2bd715092239bdea7d
Bautista Lanussegit <blanusse@udesa.edu.ar>
2021-11-01 11:55:03 -0300

    ver commit

---
+++ src/components/Menu.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+ class Menu extends Component {
+    render() {
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({})
+
+
+export default Menu;

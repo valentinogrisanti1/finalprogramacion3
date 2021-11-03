d78ac7a1d75ef809d953f9a603178abb67b5f2e1
Bautista Lanussegit <blanusse@udesa.edu.ar>
2021-11-03 19:15:49 -0300

    cambios de drawer y vistas

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
 
 
 import Home from '../screens/Home';
 import Login from '../screens/Login';
 import Register from '../screens/Register';
  
-const Drawer = createDrawerNavigator();
+const Drawer = createMaterialTopTabNavigator();
 
-class Menu extends Component {
+class Menu extends Component {  
+
+    constructor(props) {
+        super(props);
+            this.state = {
+
+            }
+    }
+
+
     render() {
         return (
             
             <NavigationContainer>
                 <Drawer.Navigator>
                     <Drawer.Screen name="Home" component={() => <Home />} />
-                    {/* <Drawer.Screen name="Login" component={() => <Login />} />
-                    <Drawer.Screen name="Register" component={() => <Register />} /> */}
+                    <Drawer.Screen name="Login" component={() => <Login />} />
+                    <Drawer.Screen name="Register" component={() => <Register />} />
                 </Drawer.Navigator>
             </NavigationContainer>
         )
---
+++ src/screens/Login.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, TextInput, TouchableOpacity } from 'react-native'
+
+ class Login extends Component {
+    render() {
+        return (
+            <View>
+               <View style={styles.contenedor}>
+                    <TextInput style={styles.lugar}
+                        onChangeText={text => this.setState({ email: text })}
+                        placeholder= 'email'
+                        keyboardType= 'email-address'
+                    />
+                <TextInput style={styles.lugar}
+                       onChangeText={(text) => this.setState({ password: text })}
+                       placeholder="password"
+                       keyboardType="email-address"
+                       secureTextEntry={true}
+                    />
+                    <TouchableOpacity  
+                    style={styles.button}
+
+                    >
+                        <Text style= {styles.textButton} >REGISTRAR</Text>
+                    </TouchableOpacity>
+                </View>      
+               
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    contenedor: {
+        alignItems: 'center',
+        paddingHorizontal: 12,
+        paddingVertical: 20, 
+    },
+
+    lugar: {   
+        height: 20,
+        paddingVertical: 15,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 6,
+        width: '100%',
+        margin: 5,
+    },
+    button: {
+        backgroundColor: "#28a745",
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: "center",
+        borderRadius: 4,
+        borderWidth: 1,
+        borderStyle: "solid",
+        borderColor: "#28a745",
+      },
+      textButton: {
+        color: "#fff",
+      },
+})
+
+export default Login;

341af97d9e61383a0f923e454a6201effe2bd68d
Bautista Lanussegit <blanusse@udesa.edu.ar>
2021-11-16 13:43:14 -0300

    mostrar posteos

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,169 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Modal } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
  class Post extends Component {
+     constructor(props){
+         super(props)
+         this.state = {
+             likes: 0,
+             liked: false,
+             mostrarModal: false,
+         }
+     }
+
+     componentDidMount(){
+         this.recivirLikes();
+     }
+
+     recivirLikes() {
+         let likes = this.props.postData.data.likes;
+         if(likes) {
+             this.setState({
+                 liked: true,
+             })
+         }
+     }
+
+     liquearPost() {
+         let post = db.collection("posteos").doc(this.props.postData.id);
+
+         post.update({
+             likes: firebase.firestore.fieldValue.arrayUnion(auth.currentUser.email)
+         })
+         .then(() => {
+             this.setState({
+                 likes: this.state.likes +1,
+                 liked: true,
+             })
+             console.log('esta likeado');
+         })
+         .catch((error) => {
+            console.error("Error updating document: ", error); 
+         })
+     }
+
+     deslikearPost() {
+         let post = db.collection("posteos").doc(this.props.postData.id);
+
+         post.update({
+             likes: firebase.firestore.fieldValue.arrayUnion(auth.currentUser.email)
+         })
+         .then(() => {
+             this.setState({
+                 likes: this.state.likes - 1,
+                 liked: false,
+             })
+             console.log('delikeado');
+         })
+         .catch((error) => {
+            console.error("Error updating document: ", error);
+        });
+     }
+
+     abrirModal() {
+         this.setState({
+             mostrarModal: true,
+         })
+     }
+
+     cerrarModal() {
+         this.setState({
+             mostrarModal: false,
+         })
+     }
+
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
+            <View style={styles.container}>
+                <Text>{this.props.postData.data.user}</Text>
+                <Text>{this.props.postData.data.decription}</Text>
+                <TouchableOpacity onPress={() => this.abrirModal()}>
+                    <Text>LIKES: {this.state.likes}</Text>
+                </TouchableOpacity>
+                {
+                    ! this.state.liked ?
+                        <TouchableOpacity style={styles.button} 
+                        onPress={() => this.liquearPost()}>
+                            <Text style={styles.textButton}> lIKEAR</Text>
+                        </TouchableOpacity>
+                        :
+                        <TouchableOpacity style={styles.button} 
+                        onPress={() => this.deslikearPost()}>
+                            <Text style={styles.textButton}> DESlIKEAR</Text>
+                        </TouchableOpacity>
+                }
+
+                {
+                    ! this.state.mostrarModal ?
+                        null
+                        :
+                            <Modal
+                                style={styles.modalContainer}
+                                visible={this.state.mostrarModal}
+                                animationType="slide"
+                                transparent={false}
+                                >
+
+                                    <TouchableOpacity onPress= {() => this.cerrarModal()} style={styles.closeModal}>
+                                        <Text>X</Text>
+                                    </TouchableOpacity>
+                                    <Text style={styles.modalText}> soy un modal</Text>
+                                
+                            </Modal>
+                }
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        marginVertical: 15,
+        shadowColor: "#ccc",
+        shadowOffset: {
+            width: 0,
+            height: 0,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+        borderRadius: 5,
+    },
+    button: {
+        backgroundColor: "#28a745",
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: "center",
+        borderRadius: 4,
+        borderWidth: 1,
+        borderStyle: "solid",
+        borderColor: "#28a745",
+    },
+    textButton: {
+        color: "#fff",
+    },
+    modalContainer: {
+        width:'100%',  
+        flex: 3,
+        alignSelf: 'center',
+        backgroundColor: "white",
+        borderColor: '#000000',
+        borderRadius: 6,
+        padding: 10,
+        backgroundColor: '#000000'
+    },
+    closeModal:{
+        alignSelf: 'flex-end',
+        padding: 10,
+        backgroundColor: '#dc3545',
+        marginTop:2,
+        borderRadius: 4,
+    },
+    modalText:{
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+})
 
 
 export default Post;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,69 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, FlatList} from 'react-native'
+import { db } from'../firebase/config' 
+
+import Post from '../components/Post'
 
 class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+        this.mostrarPost();
+    }
+
+    mostrarPost() {
+        db.collection('posteos').onSnapshot((docs) => {
+            let posteos = []
+            docs.forEach((doc) => {
+                posteos.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            })
+            this.setState({
+                posts: posteos,
+            })
+        })
+    }
+
     render() {
         return (
-            <View>
-                 <Text>Home Page</Text>
+            <View style={styles.container}>
+                <FlatList
+                    data={this.state.posts}
+                    keyExtractor={(post) => post.id}
+                    renderItem={({item}) => <Post postDAta={item}/>}
+                    />
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        textAlign: 'center',
+        padding: 10,
+    },
+    image:{
+        height:250,
+    },
+    flatList:{
+        justifyContent:'space-between'
+    },
+    touchable:{
+        padding: 4,
+        backgroundColor: '#ccc',
+        marginBottom: 10,
+        borderRadius: 4,
+    },
+    touchableText:{
+        fontWeight: 'bold'
+    }
+});
 
 export default Home;

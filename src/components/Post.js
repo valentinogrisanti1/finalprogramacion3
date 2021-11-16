ded329a9673aa1ba145a12f7da3386fd4181c8db
Bautista Lanussegit <blanusse@udesa.edu.ar>
2021-11-16 12:27:50 -0300

    hola2

---
+++ src/components/Post.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+ class Post extends Component {
+    render() {
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({})
+
+
+export default Post;
